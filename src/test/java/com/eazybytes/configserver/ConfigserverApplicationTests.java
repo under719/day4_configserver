d9d365d458281b3dc7f6031500e11828864a9cea
under76 <under76@mobigen.com>
2025-02-25 20:56:18 +0900

    first commit

---
+++ src/test/java/com/eazybytes/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eazybytes.configserver;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
